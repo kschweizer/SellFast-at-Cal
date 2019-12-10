a0d2a53846fff275e083278ee1eb0f0d1132ec0b
Jiajun Lu <jlu82@berkeley.edu>
2019-12-09 23:57:04 -0800

    tests

---
+++ SellFast/features/step_definitions/create_item_steps.rb
@@ -0,0 +1,21 @@
+
+Given("I have logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the home page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I click {string} link") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill in with item name {string} with decription {string} with Selling price {string}") do |string, string2, string3|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see my item be posted on the home page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
